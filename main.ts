23deb440ec5f41032c7bfe6ee64f782165a2004b
seiji-okura <slower.than.snails@gmail.com>
2023-05-21 10:06:55 +0900

    Initial commit

---
+++ main.ts
@@ -0,0 +1,28 @@
+"use strict";
+
+async function Greeting(name: string): Promise<number> {
+    console.log('Greeting start'); // 3
+    const hour = new Date().getHours();
+    if( hour < 12 ){
+        console.log(`Hello, ${name}`); 
+    }else{
+        return Promise.reject(new Error('too late'));
+    }
+
+    return Promise.resolve(hour);
+}
+
+await Greeting('Seiji').then((res) => {
+        console.log(res);
+        return Number(res)*2;
+    }).then((res) => {
+        console.log(res);
+        return Promise.resolve(res*2); // resolveされたPromiseを返しています
+    }).then((res) => {
+        console.log(res); //: resolveされた値で`then`が呼び出されます
+        return res*2;
+    }).catch((err) => {
+        console.log(err);
+    });
+
+console.log('global end'); // 1
